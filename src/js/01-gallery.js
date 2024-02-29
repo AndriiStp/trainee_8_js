5f22cb78d4d357e12910f922129a97a2176b4869
Andrii Stepanenko <andrii.stp@gmail.com>
2024-02-29 13:43:42 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+(function () {
+  const markup = galleryItems
+    .map(
+      ({ preview, description, original }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    )
+    .join('');
+
+  list.insertAdjacentHTML('beforeend', markup);
+})();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.open();
