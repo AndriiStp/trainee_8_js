9856833decba282fe051f6525a1fd7dac8659ae5
Andrii Stepanenko <andrii.stp@gmail.com>
2024-02-29 15:39:50 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+const currentTimeInLocal = localStorage.getItem(currentTime);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(timeupdate) {
+  localStorage.setItem(currentTime, timeupdate.seconds);
+}
+
+player.setCurrentTime(JSON.parse(currentTimeInLocal) || 0);
