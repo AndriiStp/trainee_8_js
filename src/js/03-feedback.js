2a0148c9891ef049a7b54370cc894e10cfdc4d41
Andrii Stepanenko <andrii.stp@gmail.com>
2024-03-01 16:19:13 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const FEEDBACK_KEY = 'feedback-form-state';
+
+let formData = localStorage.getItem(FEEDBACK_KEY);
+
+form.addEventListener('input', throttle(onInput, 1000));
+form.addEventListener('submit', onSubmit);
+
+getStorageValues();
+// const { enail, message } = form.elements;
+
+function onInput(e) {
+  const message = e.target.value;
+  console.log(message);
+  localStorage.setItem(FEEDBACK_KEY, message);
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_KEY);
+}
+
+function getStorageValues() {
+  if (formData) {
+    console.log(formData);
+    form.value = formData;
+  }
+}
