ddbc13e9438a4999234f694ca8c4fce40469b995
Andrii Stepanenko <andrii.stp@gmail.com>
2024-03-01 16:28:08 +0200

    64

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,28 +5,33 @@ const FEEDBACK_KEY = 'feedback-form-state';
 
 let formData = localStorage.getItem(FEEDBACK_KEY);
 
-form.addEventListener('input', throttle(onInput, 1000));
+form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onSubmit);
 
 getStorageValues();
 // const { enail, message } = form.elements;
 
 function onInput(e) {
-  const message = e.target.value;
-  console.log(message);
-  localStorage.setItem(FEEDBACK_KEY, message);
+  const { name, value } = e.target;
+  formData[name] = value;
+
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
 function onSubmit(e) {
   e.preventDefault();
+  console.log(formData);
 
-  e.currentTarget.reset();
+  form.reset();
   localStorage.removeItem(FEEDBACK_KEY);
+  formData = {};
 }
 
 function getStorageValues() {
-  if (formData) {
-    console.log(formData);
-    form.value = formData;
+  if (formData.message) {
+    form.elements.message.value = formData.message;
+  }
+  if (formData.email) {
+    form.elements.email.value = formData.email;
   }
 }
